a11d2b17b9643be40ac9186bec44d8dbd19c314b
jaketgm <jakeruns7@gmail.com>
2025-10-27 12:35:44 -0300

    "Update: 12:35 P.M. Oct. 27th"

--- app/src/main/java/com/example/exertion/ui_components/NavBar.kt
+++ app/src/main/java/com/example/exertion/ui_components/NavBar.kt
@@ -1,8 +1,18 @@
 package com.example.exertion.ui_components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import com.example.exertion.R
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.navigation.NavController
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NavBar(
     user_name: String,
@@ -11,5 +21,28 @@ fun NavBar(
     nav_controller: NavController?,
     on_profile_click: () -> Unit,
 ) {
+    val namdhinggo = FontFamily(Font(R.font.namdhinggo_regular))
+    val now = LocalDate.now()
+    val day = now.dayOfMonth
+    val suffix = when {
+        day in 11..13 -> "th"
+        day % 10 == 1 -> "st"
+        day % 10 == 2 -> "nd"
+        day % 10 == 3 -> "rd"
+        else -> "th"
+    }
+    val formatted_date = now.format(DateTimeFormatter.ofPattern("MMMM d',' yyyy")).replace(Regex("\\d+,")) {
+        match -> val number = match.value.dropLast(1).toInt()
+        "$number$suffix"
+    }
 
+    // Variant Colours
+    val dark_gradient = Brush.radialGradient(
+        colors = listOf(
+            Color(0x66000000),
+            Color(0x1A723333)
+        ),
+        center = androidx.compose.ui.geometry.Offset(0f, 0f),
+        radius = 800f
+    )
 }
